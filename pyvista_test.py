6af911c33c99d59b4074f285ce2558f5bcdeb52d
Rayna  Ney <rlney@ucdavis.edu>
2022-12-02 16:13:37 +0100

    Update pyvista_test.py

--- pyvista_test.py
+++ pyvista_test.py
@@ -20,7 +20,7 @@ def main(argv):
 
     mesh = pv.StructuredGrid(filename) #read in file
     #mesh.set_active_scalars("O2") #get O2 scalar
-    smin, smax = mesh.GetScalarRange() #get scalar range
+    #smin, smax = mesh.GetScalarRange() #get scalar range
     """
     # Create colour transfer table: mapping scalar value to colour
     opacity_function = vtk.vtkPiecewiseFunction() #a function that creates a ramp
@@ -43,26 +43,35 @@ def main(argv):
     """
 
     model = mesh
-    contours = model.contour(10, scalars='O2')
+
+    # contours for O2
+    contours = model.contour(15, scalars='O2')
     contours.array_names
+
+    contours['O2'] /= contours['O2'].max() #not sure what this line is actually doing
 
     p = pv.Plotter(shape=(1, 2))
 
-    p.subplot(0, 0)
     p.add_text('Opacity by Array')
     p.add_mesh(
         contours.copy(),
         use_transparency=True,
+        scalars='O2',
+        opacity='O2', #this is not quite right either
         cmap='bwr',
     )
 
+    # contours for fire fuel
+    contours = mesh.contour(scalars = 'rhof_1')
+    p.add_mesh(mesh.outline(), color="k")
+    p.add_mesh(contours, opacity=0.25, clim=[0, 200])
+
+    """
     p.subplot(0, 1)
     p.add_text('No Opacity')
     p.add_mesh(contours, scalars='O2', cmap='bwr')
+    """
     p.show()
-    
-    breakpoint()
-    
 
 
 if __name__ == "__main__":
